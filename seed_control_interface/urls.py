3f5fd3f84c09f5fbbc77449d67002ddae7f76ecb
Simon de Haan <simon@praekeltfoundation.org>
2016-09-29 17:11:23 +0200

    tests for exporting

--- seed_control_interface/urls.py
+++ seed_control_interface/urls.py
@@ -9,7 +9,7 @@ admin.site.site_header = os.environ.get('SEED_CONTROL_INTERFACE_TITLE',
 
 urlpatterns = patterns(
     '',
-    url(r'^admin/',  include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('ci.urls')),
 )
 urlpatterns += staticfiles_urlpatterns()
