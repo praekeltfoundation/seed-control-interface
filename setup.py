1f1986001d02c40f704563f5a1f7090a49232e96
Jason Paidoussi <jason@paidoussi.net>
2017-01-05 22:18:53 +0200

    Fix setup package

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_version(package):
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
-version = get_version('seed_scheduler')
+version = get_version('seed_control_interface')
 
 
 setup(
