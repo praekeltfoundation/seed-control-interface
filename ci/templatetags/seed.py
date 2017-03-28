a0e45ee2153659075b3822b72dc1e23dd0a2fc5e
Erik Harding <erik@praekelt.org>
2017-03-28 12:09:50 +0200

    display roles in ci

--- ci/templatetags/seed.py
+++ ci/templatetags/seed.py
@@ -8,3 +8,15 @@ register = template.Library()
 @register.simple_tag
 def current_version():
     return seed_control_interface.__version__
+
+
+@register.filter
+def cleanup(value):
+    return value.replace('_', ' ')
+
+
+@register.filter
+def check_object(value):
+    if type(value) == dict:
+        return True
+    return False
