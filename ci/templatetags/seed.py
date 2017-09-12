a1909001374eb8fe39bb14af45b3bf57b3360dc8
Alex Muller <alex@praekelt.org>
2017-09-12 23:17:54 +0100

    Improve navigation HTML and CSS
    Having the h3 heading element outside of the ul makes much more
    sense semantically.
    
    Add a template tag which massively reduces duplication in the view.

--- ci/templatetags/seed.py
+++ ci/templatetags/seed.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 import seed_control_interface
 
@@ -26,3 +27,22 @@ def is_dict(value):
 @register.filter
 def get_identity(obj):
     return obj.get(settings.IDENTITY_FIELD)
+
+
+@register.inclusion_tag('ci/includes/nav.html', takes_context=True)
+def nav_url(context, page, link_text, page_param=None):
+    if page_param is not None:
+        url = reverse(page, args=[page_param])
+    else:
+        url = reverse(page)
+
+    if context['request'].path == url:
+        html_class = 'active'
+    else:
+        html_class = ''
+
+    return {
+        'class': html_class,
+        'link_text': link_text,
+        'url': url,
+    }
